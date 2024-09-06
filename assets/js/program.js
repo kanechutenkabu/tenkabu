7e11523ec527564b1753530f22ef755e071de6bb
kanechutenkabu <kanechutenkabu@gmail.com>
2024-09-06 09:43:19 +0900

    rb_tgl

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,14 +1,11 @@
 const btn = document.querySelector("input");
 btn.addEventListener("click",tgl_dsp);
 const ancher = document.querySelectorAll("a");
-ancher[0].addEventListener("click",tgl_dsp);
 ancher[1].addEventListener("click",tgl_dsp);
 ancher[2].addEventListener("click",tgl_dsp);
 ancher[3].addEventListener("click",tgl_dsp);
 ancher[4].addEventListener("click",tgl_dsp);
 ancher[5].addEventListener("click",tgl_dsp);
-ancher[6].addEventListener("click",tgl_dsp);
-ancher[7].addEventListener("click",tgl_dsp);
 
 function tgl_dsp() {
     const hide = document.querySelector("#ul");
